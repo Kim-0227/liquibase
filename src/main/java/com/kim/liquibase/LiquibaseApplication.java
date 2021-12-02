0b9976d289094cfba8e3e2f0d9156c665429faa7
jinriguang@jujiaotech.com <a5611190A>
2021-12-02 11:02:17 +0800

    创建liquibase项目

---
+++ src/main/java/com/kim/liquibase/LiquibaseApplication.java
@@ -0,0 +1,16 @@
+package com.kim.liquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author kim
+ */
+@SpringBootApplication
+public class LiquibaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiquibaseApplication.class, args);
+    }
+
+}
